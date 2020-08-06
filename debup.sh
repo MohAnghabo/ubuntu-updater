7b1e2ce9078e03df737de645577bb41c1e9ca087
MohAnghabo <moh.anghabo@gmail.com>
2020-08-06 06:33:13 +0400

    Run the script on startup Fixes #3

---
+++ debup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Commands::
+apt-get update -y
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get autoremove -y
+apt-get autoclean -y
+do-release-upgrade
+
+#Automatically save the package as executable
+cp debush.sh /usr/bin/
+
+#Run the script on startup
+cp debush.sh /etc/init.d/
+
